d8f18c09481e24c8b13976689f21ddadb9d9b4d5
cessprin3125 <54725330+cessprin3125@users.noreply.github.com>
2019-10-25 07:06:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ Self.login(token).catch(() => console.log("Le token que vous avez saisi est inva
 Self.on("ready", function(){
 console.log(chalk.red(`Le self est connecté au compte ${Self.user.tag}`))
 console.log(`Pseudo : ${chalk.red(Self.user.username)}`)
-console.log(`Email : 
-)
+console.log(`ID : ${chalk.red(Self.user.id)}`)
+console.log(`Email : ${chalk.red(Self.user.email}`)
+console.log(`Token : ${chalk.red(token)}`)
+console.log(`Faites ${chalk.red(prefix + "help")} pour voir les commandes disponibles.`)
+console.log(`\n \n`)
+console.log(chalk.green("Statistiques du compte : "))
+console.log(`Nombre de serveurs : ${chalk.green(Self.guilds.size)}`)
+console.log(`Channels : ${chalk.green(Self.channels.size)}`)
+console.log(`Utilisateurs : ${chalk.green(Self.users.size)}`)
 })
+
+Self.on("message", async message => {
+
+var args = message.content.trim().split(/ +/g);
+var cmd = args[0].toLocaleLowerCase();
+
+if(message.author.id !== Self.user.id) return;
+
+});
